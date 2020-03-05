de6078e66164950392fdb55c837ffa3f903e1063
grfreitas <gabrielfreitas@usp.br>
2020-03-04 23:51:49 -0300

    Small changes on structure

--- bot.py
+++ bot.py
@@ -13,12 +13,17 @@ from webdriver_manager.firefox import GeckoDriverManager
 
 
 class BandecoBot:
-    """Creates a bot object that scraps the bandeco menu and uploads to twitter as a thread.
+
+    """
+    Creates a bot object that scraps the bandeco menu and uploads to twitter as a thread.
         :param meal_time: Meal time for menus, available options are 'almoco' and 'janta'
         :param weekday: Weekday name, options are 'segunda', 'terca', 'quarta', 'quinta' and 'sexta'
     """
+
     def __init__(self, meal_time, weekday):
-        self.meal_time = ''.join([meal_time, weekday.title()])
+
+        self.meal_time = meal_time
+        self.weekday = weekday
         self.codes = {
             'Central': 6,
             'Prefeitura': 7,
@@ -30,36 +35,44 @@ class BandecoBot:
 
     @staticmethod
     def _get_credentials():
+
         keys = dict(consumer_key=os.environ['CONSUMER_KEY'],
                     consumer_secret=os.environ['CONSUMER_SECRET'],
                     access_token_key=os.environ['ACCESS_TOKEN_KEY'],
                     access_token_secret=os.environ['ACCESS_TOKEN_SECRET'])
+
         return keys
 
     def _get_bandeco_menu(self, name):
+
         code = self.codes[name]
         url = f'https://uspdigital.usp.br/rucard/Jsp/cardapioSAS.jsp?codrtn={code}'
+        id_ = self.meal_time.lower() + self.weekday.lower().title()
         self.browser.get(url)
         delay = 5
+
         try:
-            WebDriverWait(self.browser, delay).until(EC.presence_of_element_located((By.ID, self.meal_time)))
+            WebDriverWait(self.browser, delay).until(EC.presence_of_element_located((By.ID, id_)))
             time.sleep(.5)  # Elements may took some time to load
-            return self.browser.find_elements_by_id(self.meal_time)[0].text
+            return self.browser.find_elements_by_id(id_)[0].text
         except TimeoutException:
             raise TimeoutError('Element took to long to load.')
 
     def _get_payload(self):
-        menus = ['Cardápios ALMOÇO / SEGUNDA (05/03/2020):']
+
+        menus = [f"Cardápios {self.meal_time.replace('C', 'Ç')} / {self.weekday}(05/03/2020):"]
+
         for name in self.codes.keys():
             menu = name + ':\n\n' + self._get_bandeco_menu(name)
             menus.append(menu)
+
         self.browser.close()
+
         return menus
 
     def post_twitter_thread(self):
 
         payload = self._get_payload()
-
         th = Threader(payload, self.api, wait=2, end_string=False)
         th.send_tweets()
 
