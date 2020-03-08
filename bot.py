d19fda07138c45376a29f9c38e29e2db176275dc
grfreitas <gabrielfreitas@usp.br>
2020-03-08 15:27:14 -0300

    Changes payload style

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import os
 import time
 
@@ -13,37 +14,42 @@ from webdriver_manager.firefox import GeckoDriverManager
 
 
 class BandecoBot:
-
     """
     Creates a bot object that scraps the bandeco menu and uploads to twitter as a thread.
         :param meal_time: Meal time for menus, available options are 'almoco' and 'janta'
         :param weekday: Weekday name, options are 'segunda', 'terca', 'quarta', 'quinta' and 'sexta'
     """
 
-    def __init__(self, meal_time, weekday):
+    def __init__(self, meal_time, weekday, date):
         self.meal_time = meal_time
         self.weekday = weekday
+        self.date = date
         self.codes = {
             'Central': 6,
             'Prefeitura': 7,
             'Física': 8,
             'Química': 9,
         }
-        self.api = TwitterAPI(**self._get_credentials())
+        self.api = TwitterAPI(**self._get_twitter_credentials())
         self.browser = Firefox(executable_path=GeckoDriverManager().install())
 
     @staticmethod
-    def _get_credentials():
-        keys = dict(consumer_key=os.environ['CONSUMER_KEY'],
-                    consumer_secret=os.environ['CONSUMER_SECRET'],
-                    access_token_key=os.environ['ACCESS_TOKEN_KEY'],
-                    access_token_secret=os.environ['ACCESS_TOKEN_SECRET'])
-        return keys
+    def _get_twitter_credentials():
+        credentials = dict(consumer_key=os.environ['API_KEY'],
+                           consumer_secret=os.environ['API_SECRET_KEY'],
+                           access_token_key=os.environ['ACCESS_TOKEN'],
+                           access_token_secret=os.environ['ACCESS_TOKEN_SECRET'])
+        return credentials
 
     def _get_bandeco_menu(self, name):
         code = self.codes[name]
         url = f'https://uspdigital.usp.br/rucard/Jsp/cardapioSAS.jsp?codrtn={code}'
-        id_ = self.meal_time.lower() + self.weekday.lower().title()
+
+        id_ = self.meal_time.replace('ç', 'c').lower() + \
+              self.weekday.replace('á', 'a').replace('ç', 'c').lower().title()
+
+        self.id_ = id_
+
         self.browser.get(url)
         delay = 5
         try:
@@ -51,18 +57,28 @@ class BandecoBot:
             time.sleep(.5)  # Elements may took some time to load
             return self.browser.find_elements_by_id(id_)[0].text
         except TimeoutException:
-            raise TimeoutError('Element took to long to load.')
+            return ''
 
     def _get_payload(self):
-        menus = [f"Cardápios {self.meal_time.replace('C', 'Ç')} / {self.weekday}(05/03/2020):"]
+        menus = ["Cardápios USP Butantã - "
+                 f"{self.meal_time.title()} "
+                 f"{self.weekday.title()} "
+                 f"({str(self.date.day).zfill(2)}/{str(self.date.month).zfill(2)}/{self.date.year}):"]
+
         for name in self.codes.keys():
-            menu = name + ':\n\n' + self._get_bandeco_menu(name)
-            menus.append(menu)
+            menu = self._get_bandeco_menu(name)
+            if menu != '':
+                menu = name + ':\n\n' + self._get_bandeco_menu(name)
+                menus.append(menu)
+
         self.browser.close()
         return menus
 
     def post_twitter_thread(self):
         payload = self._get_payload()
-        th = Threader(payload, self.api, wait=2, end_string=False)
-        th.send_tweets()
+
+        if len(payload) > 1:
+            th = Threader(payload, self.api, wait=2, end_string=False)
+            th.send_tweets()
+
         return payload
